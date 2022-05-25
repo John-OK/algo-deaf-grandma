0e22e75a88b249d410de315086aae9b57e88cac3
JohnOK <john.ok.esp@gmail.com>
2022-05-25 21:50:21 +0200

    added python version of deaf_grandma

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,18 @@
 def deaf_grandma():
+    number_goodbyes = 0
+    user_response = input ("HEY KID!\n")
 
+    while(number_goodbyes < 2):
+        if(user_response == "GOODBYE!" and number_goodbyes == 0):
+            user_response = input("LEAVING SO SOON?\n")
+            number_goodbyes += 1
+        elif(user_response == "GOODBYE!" and number_goodbyes == 1):
+            print("LATER, SKATER!")
+            number_goodbyes += 1
+        elif(not user_response.isupper()):
+            user_response = input("SPEAK UP, KID!\n")
+        else:
+            user_response = input("NO, NOT SINCE 1946!\n")
 
 
 deaf_grandma()
