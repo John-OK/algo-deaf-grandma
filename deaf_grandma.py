a04957b80e6af812039a12a0d74b2fc5ecde6d53
John OKeeffe <john.ok.esp@gmail.com>
2022-05-29 23:10:15 -0500

    fixed no input giving wrong response

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -9,8 +9,10 @@ def deaf_grandma():
         elif(user_response == "GOODBYE!" and number_goodbyes == 1):
             print("LATER, SKATER!")
             number_goodbyes += 1
-        elif(not user_response.isupper()):
+        elif(not user_response.isupper() and len(user_response) > 0):
             user_response = input("SPEAK UP, KID!\n")
+        elif user_response == "":
+            user_response = input("WHAT?!\n")
         else:
             user_response = input("NO, NOT SINCE 1946!\n")
 
