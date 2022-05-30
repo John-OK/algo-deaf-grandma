7516ac63bbd132cfef589794fd91e8b93977d04a
John OKeeffe <john.ok.esp@gmail.com>
2022-05-29 23:12:49 -0500

    removed non-pythonic parentheses

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -2,14 +2,14 @@ def deaf_grandma():
     number_goodbyes = 0
     user_response = input ("HEY KID!\n")
 
-    while(number_goodbyes < 2):
-        if(user_response == "GOODBYE!" and number_goodbyes == 0):
+    while number_goodbyes < 2:
+        if user_response == "GOODBYE!" and number_goodbyes == 0:
             user_response = input("LEAVING SO SOON?\n")
             number_goodbyes += 1
-        elif(user_response == "GOODBYE!" and number_goodbyes == 1):
+        elif user_response == "GOODBYE!" and number_goodbyes == 1:
             print("LATER, SKATER!")
             number_goodbyes += 1
-        elif(not user_response.isupper() and len(user_response) > 0):
+        elif not user_response.isupper() and len(user_response) > 0:
             user_response = input("SPEAK UP, KID!\n")
         elif user_response == "":
             user_response = input("WHAT?!\n")
