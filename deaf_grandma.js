79236420b3ae3e461dcdd1a4a5c86d9be4fa0ea6
John OKeeffe <john.ok.esp@gmail.com>
2022-05-30 10:01:07 -0500

    added JS version

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -2,6 +2,21 @@ function deafGrandma() {
     let goodbyes = 0;
     let input = window.prompt('HEY KID!');
     while (goodbyes < 2) {
+        if (input == "GOODBYE!") {
+            if (goodbyes == 0) {
+                input = window.prompt('LEAVING SO SOON?')
+                goodbyes++;
+            } else {
+                window.prompt('LATER, SKATER!')
+                goodbyes++;
+            }
+        }else if (input == "") {
+            input = window.prompt('WHAT?!')
+        }else if (input != input.toUpperCase()) {
+            input = window.prompt('SPEAK UP, KID!')
+        }else {
+            input = window.prompt('NO, NOT SINCE 1946!')
+        }
     }
 }
 
